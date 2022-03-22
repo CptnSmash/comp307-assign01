8f317d0f3a7f4273783e059c895456e40d022b29
dasanmich <dasanmich@ecs.vuw.ac.nz>
2022-03-22 15:48:19 +1300

    Initial commit, very basic wine class

---
+++ Wine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Wine
+{
+	private decimal alcohol;
+	private decimal malic;
+	private decimal ash;
+	private decimal ashAlkalinity;
+	private decimal magnesium;
+	private decimal totalPhenols;
+	private decimal flavanoids;
+	private decimal nfnPhenols;
+	private decimal proanthocyanins;
+	private decimal colorIntensity;
+	private decimal hue;
+	private decimal od;
+	private decimal proline;
+	//The class of the wine. Called category here as class is a restricted keyword.
+	private int category;
+
+	public Wine()
+	{
+
+	}
+}
