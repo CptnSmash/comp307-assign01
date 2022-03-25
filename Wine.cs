504d4b7d295a8ba6e085e2864af88eec661aaea2
dasanmich <dasanmich@ecs.vuw.ac.nz>
2022-03-25 18:13:19 +1300

    Update documentation on Wine

--- Wine.cs
+++ Wine.cs
@@ -1,66 +1,50 @@
 ﻿using System;
+using System.Diagnostics;
 
 public class Wine
 {
-	private decimal alcohol { get => alcohol; set { alcohol = value; } };
-	private decimal malic { get => malic; set { malic = value; } };
-	private decimal ash { get => ash; set { ash = value; } };
-	private decimal ashAlkalinity { get => ashAlkalinity; set { ashAlkalinity = value; } };
-	private decimal magnesium { get => magnesium; set { magnesium = value; } };
-	private decimal totalPhenols { get => totalPhenols; set { totalPhenols = value; } };
-	private decimal flavanoids { get => flavanoids; set { flavanoids = value; } };
-	private decimal nfnPhenols { get => nfnPhenols; set { nfnPhenols = value; } };
-	private decimal proanthocyanins { get => proanthocyanins; set { proanthocyanins = value; } };
-	private decimal colorIntensity { get => colorIntensity; set { colorIntensity = value; } };
-	private decimal hue { get => hue; set { hue = value; } };
-	private decimal od { get => od; set { od = value; } };
-	private decimal proline { get => proline; set { proline = value; } };
+	/// <summary>
+    /// 
+    /// </summary>
+	private decimal[] values;
+	public decimal[] Values {
+		get => values;
+		set { values = value; }; 
+	}
 	//The class of the wine. Called category here as class is a restricted keyword.
-	private int category { get => category; set { category = value; } };
+	private int category;
+	public int Category {
+		get => category;
+		set { category = value; };
+	}
 
 	//Main constructor - for test and actual use
-	public Wine(decimal alcohol, decimal malic, decimal ash, decimal ashAlkalinity, decimal magnesium, decimal totalPhenols, decimal flavanoids, decimal nfnPhenols, decimal proanthocyanins, decimal colorIntensity, decimal hue, decimal od, decimal proline)
-	{
-		this.alcohol = alcohol;
-		this.malic = malic;
-		this.ash = ash;
-		this.ashAlkalinity = ashAlkalinity;
-		this.magnesium = magnesium;
-		this.totalPhenols = totalPhenols;
-		this.flavanoids = flavanoids;
-		this.nfnPhenols = nfnPhenols;
-		this.proanthocyanins = proanthocyanins;
-		this.colorIntensity = colorIntensity;
-		this.hue = hue;
-		this.od = od;
-		this.proline = proline;
+	public Wine(decimal[] values) {
+		this.values = values;
 	}
 
 	//Training constructor - adds category information
-	public Wine(decimal alcohol, decimal malic, decimal ash, decimal ashAlkalinity, decimal magnesium, decimal totalPhenols, decimal flavanoids, decimal nfnPhenols, decimal proanthocyanins, decimal colorIntensity, decimal hue, decimal od, decimal proline, int category)
-	{
-		this.alcohol = alcohol;
-		this.malic = malic;
-		this.ash = ash;
-		this.ashAlkalinity = ashAlkalinity;
-		this.magnesium = magnesium;
-		this.totalPhenols = totalPhenols;
-		this.flavanoids = flavanoids;
-		this.nfnPhenols = nfnPhenols;
-		this.proanthocyanins = proanthocyanins;
-		this.colorIntensity = colorIntensity;
-		this.hue = hue;
-		this.od = od;
-		this.proline = proline;
+	public Wine(decimal[] values, int category) {
+		this.values = values;
 		this.category = category;
 	}
 
-	public double distanceTo(Wine other) {
-		return null;
-	}
-
-	public bool categorise(int category) { 
-		this.category = category;
-		return true;
+	/// <summary>
+    /// Returns the Euclidean distance between this Wine and another. Used for finding the nearest other Wines for k-Nearest Neightbour algorithm.
+    /// </summary>
+    /// <param name="other">The Wine to find the distance to.</param>
+    /// <returns>A decimal type number representing the distance between the two Wines.</returns>
+	public decimal distanceTo(Wine other) {
+		decimal[] otherValues = other.Values;
+		if (values.Length != otherValues.Length) {
+			throw new ArgumentException("Cannot find distance between points: Values arrays have differnt lengths.");
+		}
+		decimal dist = 0.0;
+		for (int i = 0; i < values.Length; i++) {
+			dist += Math.Pow(otherValues[i] - values[i], 2);
+		}
+		dist = Math.Sqrt(dist);
+		Debug.Assert(dist >= 0);
+		return dist;
 	}
 }
